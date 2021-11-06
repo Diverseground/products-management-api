43865f3f995e0915bf80d82c45717645ea6c61a6
Frank Krogh <diversifiedground@gmail.com>
2021-11-06 01:47:50 -0500

    swagger documentation added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -26,8 +30,9 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Products API</h1>");
+  res.send("<h1>Products API</h1><a href='/api-docs'>Documentation<a>");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/products", authenticateUser, productRouter);
